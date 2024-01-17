9e589b3ca0f07038282902d362c8b53b11cd810e
douaa <chayebdouaa26@gmail.com>
2024-01-17 12:44:26 +0100

    firs-commit

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart/cart.service';
 import { Cart } from '../shared/models/cart';
 import { CartItem } from '../shared/models/CartItem';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.css']
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit{
 
   cart!:Cart;
-constructor(private cartService:CartService){
+constructor(private cartService:CartService ,private foodService:FoodService){
+  let foods=foodService.getAll();
+  cartService.AddToCart(foods[1]);
+  cartService.AddToCart(foods[2]);
+  cartService.AddToCart(foods[3]);
+  cartService.AddToCart(foods[4]);
 
   this.setCart();
 }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 removeFromcart(CartItem:CartItem){
   this.cartService.removeFromCart(CartItem.food.id);
   this.setCart();
